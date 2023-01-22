8287f39aa46ba8b7e6bed434ad9d6d8bde4a0139
Jackson Brienen <jacksonbrienen@gmail.com>
2023-01-22 08:01:58 -0800

    Added Example from First Video

---
+++ Example/src/SimpleGame.java
@@ -0,0 +1,33 @@
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+public class SimpleGame extends BasicGame{
+
+	public SimpleGame(String title) {
+		super(title);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void render(GameContainer container, Graphics g) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void init(GameContainer container) throws SlickException {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
+		
+	}
+
+	@Override
+	public void update(GameContainer container, int delta) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
